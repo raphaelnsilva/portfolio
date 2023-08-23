3b2fd169ea58b283b962afcd2d55e737568baf76
Raphael Silva <129803784+raphaelnsilva@users.noreply.github.com>
2023-08-23 16:46:20 -0300

    mudanças

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
-// Hooks
-import './App.css';
+import './App.css'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-// Pages
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-// Components
-{/* import Navigation from './components/Navbar/Navigation' */}
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
+import { createContext, useState } from 'react';
+//pages
+import Home from './pages/Home'
+import About from './pages/About'
+
+
+export const ThemeContext = createContext(null)
+
 
 function App() {
 
+  const [theme, setTheme] = useState("dark")
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"))
+  };
+  
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route path='/' element={<Home />}/>
-            <Route path='/about' element={<About />} />
-          </Routes>
-        </div>
-        <Footer/>
-      </BrowserRouter>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <BrowserRouter> 
+          <div className="container" id={theme}>
+          <Navbar />
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+            </Routes>
+          </div>
+          <Footer/>
+        </BrowserRouter>
+      </ThemeContext.Provider>
     </>
   )
 }
 
-export default App;
+export default App
