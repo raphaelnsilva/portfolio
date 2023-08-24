d6237b64173a9ddee1677033b2a36c17e9f02b12
Raphael Silva <129803784+raphaelnsilva@users.noreply.github.com>
2023-08-24 19:25:54 -0300

    mudanças

---
+++ src/pages/Experience.jsx
@@ -0,0 +1,14 @@
+import './Experience.css'
+
+const Experience = () => {
+  return (
+    <section id='experience'>
+      <div className='experience'>
+        <h2>Sobre o <span>Raphael</span></h2>
+        <p>Este projeto consiste em um portfolio feito com React e alguma bibliotecas para auxiliar na criação das animações.</p>
+      </div>
+    </section>
+  )
+}
+
+export default Experience;
