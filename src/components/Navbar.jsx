c5f4442a66c90a2c5ebdfb0b27e0b18c8bbbfff2
Raphael Silva <129803784+raphaelnsilva@users.noreply.github.com>
2023-08-23 21:10:50 -0300

    finalizado a navbar e a primeira pagina

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,11 +19,18 @@ const Navbar = () => {
     size='25px' 
     onClick={() => setOpen(!open)}
   />
-
   const closeIcon = <IoMdClose
     className='closeIcon'
     size='25px'
     onClick={() => setOpen(!open)}
+  />
+  const gitIcon = <FiGithub
+    className='gitIcon'
+    size='25px'
+  />
+  const linkedinIcon = <FiLinkedin
+    className='linkedinIcon'
+    size='25px'
   />
 
   return (
@@ -32,6 +39,7 @@ const Navbar = () => {
       <Link href='#home' className='brand'>
         <span>Raphael</span>
       </Link>
+      
       <ul className='menuDesktop'>
         <Link to='about' smooth={true} duration={500} onClick={() => setOpen(!open)}>
           <li>Sobre Mim</li>
@@ -46,6 +54,14 @@ const Navbar = () => {
           <li>Fale Comigo</li>
         </Link>
       </ul>
+      <div className="iconsDesktop">
+        <a href='https://github.com/raphaelnsilva' target='_blank'>
+          <li>{gitIcon}</li>
+        </a>
+        <a href='https://github.com/raphaelnsilva' target='_blank'>
+          <li>{linkedinIcon}</li>
+        </a>
+      </div>
       {open ? closeIcon : openIcon}
       {open && (
         <>
@@ -62,6 +78,14 @@ const Navbar = () => {
             <Link onClick={() => setOpen(!open)}>
               <li>Fale Comigo</li>
             </Link>
+            <div className="icons">
+              <a href='https://github.com/raphaelnsilva' target='_blank'>
+                {gitIcon}
+              </a>
+              <a href='https://github.com/raphaelnsilva' target='_blank'>
+                {linkedinIcon}
+              </a>
+            </div>
           </ul>
         </>
       )}
