357d0e8601ceba29306cdb431d406a30bdac05ae
Raphael Silva <129803784+raphaelnsilva@users.noreply.github.com>
2023-09-19 15:10:26 -0300

    mais algumas mudanças

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,7 +1,7 @@
-import './Experience.css'
+import styles from './modules/Experience.module.css'
 
 const ExperienceCard = ({imageSrc, title, description}) => (
-  <div className='cardsEx'>
+  <div className={styles.card}>
     <div>
       <img src={imageSrc} alt={title} />
       <h3>{title}</h3>
@@ -13,34 +13,36 @@ const ExperienceCard = ({imageSrc, title, description}) => (
 const Experience = () => {
   return (
     <section id='experience'>
-      <h1>Tecnologias usadas</h1>
-      <div className="contentEx">
-        <ExperienceCard
-          imageSrc='./cssImage.png'
-          title='CSS3'
-          description='CSS3 é uma linguagem de estilo usada para descrever a apresentação dos elementos da página os estilizando.'
-        />
-        <ExperienceCard
-          imageSrc='./htmlImage.png'
-          title='HTML5'
-          description='HTML5 é uma linguagem de marcação de hipertexto, que usamos para marcar os elementos da página e construí-la.'
-        />
-        <ExperienceCard
-          imageSrc='./javascriptImage.png'
-          title='JavaScript'
-          description='JavaScript é uma linguagem de programação interpretada usada no desenvolvimento web para descrever funcionalidades e comportamentos da página.'
-        />
-        <ExperienceCard
-          imageSrc='./reactImage2.png'
-          title='React'
-          description='O React é uma biblioteca front-end JavaScript de código aberto com foco em criar interfaces de usuário em páginas web'
-        />
-        <ExperienceCard
-          imageSrc='./nodeImage.png'
-          title='Node'
-          description='Software de código aberto, multiplataforma, baseado no interpretador V8 do Google. Permite a execução de códigos JavaScript fora do navegador web.'
-        />
-      </div>
+      <main className={styles.main}>
+        <h1>Tecnologias usadas</h1>
+        <div className={styles.content}>
+          <ExperienceCard
+            imageSrc='./cssImage.png'
+            title='CSS3'
+            description='CSS3 é uma linguagem de estilo usada para descrever a apresentação dos elementos da página os estilizando.'
+          />
+          <ExperienceCard
+            imageSrc='./htmlImage.png'
+            title='HTML5'
+            description='HTML5 é uma linguagem de marcação de hipertexto, que usamos para marcar os elementos da página e construí-la.'
+          />
+          <ExperienceCard
+            imageSrc='./javascriptImage.png'
+            title='JavaScript'
+            description='JavaScript é uma linguagem de programação interpretada usada no desenvolvimento web para descrever funcionalidades e comportamentos da página.'
+          />
+          <ExperienceCard
+            imageSrc='./reactImage2.png'
+            title='React'
+            description='O React é uma biblioteca front-end JavaScript de código aberto com foco em criar interfaces de usuário em páginas web'
+          />
+          <ExperienceCard
+            imageSrc='./nodeImage.png'
+            title='Node'
+            description='Software de código aberto, multiplataforma, baseado no interpretador V8 do Google. Permite a execução de códigos JavaScript fora do navegador web.'
+          />
+        </div>
+      </main>
     </section>
   )
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,12 +1,12 @@
 import {FaProjectDiagram, FaExternalLinkAlt} from 'react-icons/fa'
-import './Projects.css';
+import styles from './modules/Projects.module.css';
 
 const ProjectCard = ({title, imageSrc, description, projectRepo, projectLink}) => (
-  <div className='cards'>
+  <div className={styles.cards}>
     <h2>{title}</h2>
     <img src={imageSrc} alt={title} />
     <p>{description}</p>
-    <div className="cardLinks">
+    <div className={styles.cards_links}>
       <a href={projectRepo} target='_blank'>
         <FaProjectDiagram size='20px'/>
         <h3>Repositório</h3>
@@ -22,30 +22,32 @@ const ProjectCard = ({title, imageSrc, description, projectRepo, projectLink}) =
 const Projects = () => {
   return (
     <section id='projects'>
-      <h1>Projetos</h1>
-      <div className='content'>
-        <ProjectCard
-          title='BLOG'
-          imageSrc='./blog.png'
-          description='Projeto desenvolvido em React com a utilização do Firebase para gerenciar as autenticações.'
-          projectRepo='https://github.com/raphaelnsilva/blog'
-          projectLink='https://raphaelnsilva.github.io/blog'
-        />
-        <ProjectCard
-          title='Buscador de CEP'
-          imageSrc='./cep.png'
-          description='Este site utiliza uma API para colher as informações dos endereços e mostrar ao usuário.'
-          projectRepo='https://github.com/raphaelnsilva/buscador'
-          projectLink='https://raphaelnsilva.github.io/buscador'
-        />
-        <ProjectCard
-          title='Palavra Secreta'
-          imageSrc='./secretword.png'
-          description='Este foi meu primeiro projeto com React, para forçar a utilização de hooks como useCallback, useEffect, useState.'
-          projectRepo='https://github.com/raphaelnsilva/secretword'
-          projectLink='https://raphaelnsilva.github.io/secretword'
-        />
-      </div>
+      <main className={styles.main}>
+        <h1>Projetos</h1>
+        <div className={styles.content}>
+          <ProjectCard
+            title='BLOG'
+            imageSrc='./blog.png'
+            description='Projeto desenvolvido em React com a utilização do Firebase para gerenciar as autenticações.'
+            projectRepo='https://github.com/raphaelnsilva/blog'
+            projectLink='https://raphaelnsilva.github.io/blog'
+          />
+          <ProjectCard
+            title='Buscador de CEP'
+            imageSrc='./cep.png'
+            description='Este site utiliza uma API para colher as informações dos endereços e mostrar ao usuário.'
+            projectRepo='https://github.com/raphaelnsilva/buscador'
+            projectLink='https://raphaelnsilva.github.io/buscador'
+          />
+          <ProjectCard
+            title='Palavra Secreta'
+            imageSrc='./secretword.png'
+            description='Este foi meu primeiro projeto com React, para forçar a utilização de hooks como useCallback, useEffect, useState.'
+            projectRepo='https://github.com/raphaelnsilva/secretword'
+            projectLink='https://raphaelnsilva.github.io/secretword'
+          />
+        </div>
+      </main>
     </section>
   )
 }
