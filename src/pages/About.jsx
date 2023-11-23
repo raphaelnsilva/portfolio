441ef0b6e259b3f94d428263b61ce8a68b90315c
Raphael Silva <129803784+raphaelnsilva@users.noreply.github.com>
2023-11-23 12:03:37 -0300

    changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -28,31 +28,35 @@ const About = () => {
       <section className={styles.container}>
         <ComponentAnimation description='Sobre Mim'/>
         <article className={styles.containerContent}>
-          <p data-aos="flip-left">Muito prazer! Tenho 24 anos, moro em São Paulo, Brasil. Amo programação, tenho isso como meu principal hobbie. Amo ler, gosto de criar novas soluções e expor minhas opinião. Estou cursando ADS a distância, atualmente no 4# semestre.</p>
-          <p data-aos="flip-right">Tenho experiencia em desenvolvimento front-end, utilizando React e Next, sólida base em HTML e CSS. Também possuo experiência com Git e GitHub, ferramentas essenciais para o controle de versão e colaboração.</p>
-          <p data-aos="flip-left">Já utilizei sistemas de banco de dados como SQLite, MongoDB e Firebase para desenvolver algumas das minhas aplicações. Meu objetivo é evoluir para me tornar um programador Full Stack e estou confiante de que, com minha determinação e dedicação, alcançarei esse objetivo.</p>
-        </article>
-        {/* <article className={styles.talktomeContent}>
-          <div 
-            className={`${styles.emailButton} 
-            ${copied ? 'copied' : ''}`}
-            onClick={handleEmailClick}
-          >
-            <MdOutlineContentCopy /> 
-            <p>raphaelnunessilva@hotmail.com</p>
+          <div>
+            <p data-aos="flip-left">Muito prazer! Tenho 24 anos, moro em São Paulo, Brasil. Amo programação, tenho isso como meu principal hobbie. Amo ler, gosto de criar novas soluções e expor minhas opinião. Estou cursando ADS a distância, atualmente no 4# semestre.</p>
+            <p data-aos="flip-right">Tenho experiencia em desenvolvimento front-end, utilizando React e Next, sólida base em HTML e CSS. Também possuo experiência com Git e GitHub, ferramentas essenciais para o controle de versão e colaboração.</p>
+            <p data-aos="flip-left">Já utilizei sistemas de banco de dados como SQLite, MongoDB e Firebase para desenvolver algumas das minhas aplicações. Meu objetivo é evoluir para me tornar um programador Full Stack e estou confiante de que, com minha determinação e dedicação, alcançarei esse objetivo.</p>
           </div>
-          {copied &&
-            <p className={styles.copySuccess}>
-              Copiado com sucesso! <AiOutlineCheck/>
-            </p>
-          }
-          <a className={styles.whatsButton} 
-            href="https://wa.me/55986623604" 
-            target='_blank'>
-            <FaWhatsapp/>
-            <p>WhatsApp</p>
-          </a>
-        </article> */}
+          <div className={styles.contactButtons}>
+            <a 
+              className={styles.whatsButton} 
+              href="https://wa.me/55986623604" 
+              target='_blank'
+            >
+              <FaWhatsapp/>
+              <p>WhatsApp</p>
+            </a>
+            <div 
+              className={`${styles.emailButton} 
+              ${copied ? 'copied' : ''}`}
+              onClick={handleEmailClick}
+            >
+              <MdOutlineContentCopy /> 
+              <span>raphaelnunessilva@hotmail.com</span>
+            </div>
+            {copied &&
+              <span className={styles.copySuccess}>
+                Copiado com sucesso! <AiOutlineCheck/>
+              </span>
+            }
+          </div>
+        </article>
       </section>
     </main>
   )
